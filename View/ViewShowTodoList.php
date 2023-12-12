a0b513e20cdcb89f64d22469c6f062d36813ffa1
Fikry Aziz Septian <azizfikry15@gmail.com>
2023-12-12 11:22:52 +0700

    create view show todolist

--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -1,5 +1,32 @@
 <?php
 
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
+require_once "../Helper/Input.php";
+
 function viewShowTodoList()
 {
+  while (true) {
+    showTodoList();
+
+    echo "MENU" . PHP_EOL;
+    echo "1. Tambah Todo";
+    echo "2. Hapus Todo";
+    echo "x. Hapus Todo";
+
+    $pilihan = input("Pilih");
+
+    if ($pilihan == 1) {
+      viewAddTodoList();
+    } else if ($pilihan == 2) {
+      viewRemoveTodoList();
+    } elseif ($pilihan == "x") {
+      break;
+    } else {
+      echo "Pilihan tidak dimengerti" . PHP_EOL;
+    }
+  }
+
+  echo "Sampai Jumpa Lagi" . PHP_EOL;
 }
